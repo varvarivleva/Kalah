a85b795e00c96b6610461a5f49fb38319346bd74
varvarivleva <v.katasova@gmail.com>
2024-12-15 19:34:22 +0300

    [ALL] Добавление взаимодействия клиент-сервер

--- Kalah_client/Kalah_client/Kalah_client/MainWindow.xaml.cs
+++ Kalah_client/Kalah_client/Kalah_client/MainWindow.xaml.cs
@@ -1,28 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Kalah_client
+namespace KalahClient
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Socket _socket;
+        private Thread _receiveThread;
+
         public MainWindow()
         {
             InitializeComponent();
+            ConnectToServer();
+        }
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _socket.Connect("192.168.1.101", 4563);
+                Log("Подключено к серверу.");
+                _receiveThread = new Thread(ReceiveMessages);
+                _receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                Log($"Ошибка подключения: {ex.Message}");
+            }
+        }
+
+        private void PlayWithPlayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage("PLAY_WITH_PLAYER");
+        }
+
+        private void PlayWithComputerButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage("PLAY_WITH_COMPUTER");
+        }
+
+        private void ReceiveMessages()
+        {
+            byte[] buffer = new byte[1024];
+            try
+            {
+                while (true)
+                {
+                    int bytesRead = _socket.Receive(buffer);
+                    if (bytesRead == 0) break;
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Dispatcher.Invoke(() => Log($"Сервер: {message}"));
+
+                    // Обработка сообщений сервера
+                    if (message.StartsWith("START_GAME_WITH_PLAYER"))
+                    {
+                        Dispatcher.Invoke(() => Log("Игра с игроком началась."));
+                    }
+                    else if (message.StartsWith("START_GAME_WITH_COMPUTER"))
+                    {
+                        Dispatcher.Invoke(() => Log("Игра с компьютером началась."));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() => Log($"Ошибка: {ex.Message}"));
+            }
+            finally
+            {
+                _socket.Close();
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            _socket.Send(data);
+        }
+
+        private void Log(string message)
+        {
+            LogTextBox.Text += $"{message}\n";
         }
     }
 }
--- Kalah_server/Kalah_server/Kalah_server/Program.cs
+++ Kalah_server/Kalah_server/Kalah_server/Program.cs
@@ -1,2 +1,130 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Серверная часть с поддержкой нескольких игроков
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class KalahServer
+{
+    private static List<Socket> clients = new List<Socket>();
+    private static Dictionary<Socket, Socket> playerPairs = new Dictionary<Socket, Socket>();
+    private static object lockObj = new object();
+
+    static void Main()
+    {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        serverSocket.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.101"), 4563));
+        serverSocket.Listen(10);
+        Console.WriteLine("Сервер запущен, ожидает подключения...");
+
+        try
+        {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                Console.WriteLine("Новый клиент подключен.");
+                Thread clientThread = new Thread(() => HandleClient(clientSocket));
+                clientThread.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+        finally
+        {
+            serverSocket.Close();
+        }
+    }
+
+    static void HandleClient(Socket clientSocket)
+    {
+        lock (lockObj)
+        {
+            clients.Add(clientSocket);
+        }
+
+        try
+        {
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                int bytesRead = clientSocket.Receive(buffer);
+                if (bytesRead == 0) break;
+
+                string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Получено от клиента: {receivedData}");
+
+                if (receivedData == "PLAY_WITH_PLAYER")
+                {
+                    lock (lockObj)
+                    {
+                        Socket opponent = FindOpponent(clientSocket);
+                        if (opponent != null)
+                        {
+                            playerPairs[clientSocket] = opponent;
+                            playerPairs[opponent] = clientSocket;
+
+                            SendMessage(clientSocket, "START_GAME_WITH_PLAYER");
+                            SendMessage(opponent, "START_GAME_WITH_PLAYER");
+                        }
+                        else
+                        {
+                            SendMessage(clientSocket, "WAITING_FOR_PLAYER");
+                        }
+                    }
+                }
+                else if (receivedData == "PLAY_WITH_COMPUTER")
+                {
+                    SendMessage(clientSocket, "START_GAME_WITH_COMPUTER");
+                }
+                else if (playerPairs.ContainsKey(clientSocket))
+                {
+                    // Пересылка хода другому игроку
+                    Socket opponent = playerPairs[clientSocket];
+                    SendMessage(opponent, receivedData);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+        finally
+        {
+            lock (lockObj)
+            {
+                clients.Remove(clientSocket);
+                if (playerPairs.ContainsKey(clientSocket))
+                {
+                    Socket opponent = playerPairs[clientSocket];
+                    playerPairs.Remove(clientSocket);
+                    playerPairs.Remove(opponent);
+                    SendMessage(opponent, "PLAYER_DISCONNECTED");
+                }
+            }
+            clientSocket.Close();
+        }
+    }
+
+    static Socket FindOpponent(Socket client)
+    {
+        foreach (Socket otherClient in clients)
+        {
+            if (otherClient != client && !playerPairs.ContainsKey(otherClient))
+            {
+                return otherClient;
+            }
+        }
+        return null;
+    }
+
+    static void SendMessage(Socket client, string message)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        client.Send(data);
+    }
+}
